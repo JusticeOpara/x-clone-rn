af35c449027cc578ac41790d761aa78979d800d5
Justice Opara <justiceopara33@gmail.com>
2025-09-25 08:29:31 +0100

    rn basic setup

---
+++ moblie/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+// import "../global.css"
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-300">
+      <Text className="flex justify-center items-end font-bold text-blue-500">Justice you are welcomed</Text>
+     
+    </View>
+  );
+}
+
+
